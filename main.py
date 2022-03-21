19449ba02f1222a81109b05df0d60eef4a61fcd4
Daniel Toll <ttoda@lnu.se>
2022-03-21 16:35:15 +0100

    did I fuck things up?

--- main.py
+++ main.py
@@ -10,6 +10,29 @@ from view.CompositeView import CompositeView
 from view.SerialConsoleView import SerialConsoleView
 from LoRaMeshLibrary.PycomInterface import PycomInterface
 
+import secrets
+
+from network import WLAN
+from MicropythonGitDeploy.gitDeploy import gitDeploy
+from MicropythonGitDeploy.DeployServer import DeployServer
+import time
+
+
+
+wlan = WLAN()
+wlan.connect(ssid=secrets.ssid, auth=(WLAN.WPA2, secrets.pwa))
+print('connecting..',end='')
+while not wlan.isconnected():
+    time.sleep(1)
+    print('.',end='')
+
+print('connected' + str(wlan.ifconfig()[0]))
+
+
+filesToKeep=["secrets.py"]
+ignoreUpload=[".gitmodules", ".gitignore", 'pymakr.conf', 'LICENSE', "README.md", "OTADeploy.sh"]
+gd = gitDeploy("dntoll", "LoRaMeshLoPyConsole", filesToKeep, ignoreUpload)
+ds = DeployServer(gd, wlan, 80)
 
 view = CompositeView()
 #view.add(RGBView())
